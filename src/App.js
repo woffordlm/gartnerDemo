26ae972d25432be6d624f88688346dd58114b655
woffordlm <lukewofford@gmail.com>
2022-06-13 18:04:57 -0400

    adding highchart to app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Highcharts } from "highcharts/modules/stock";
+import HighchartsReact from 'highcharts-react-official';
 
 function App() {
+
+  function testing (stockData) {
+    console.log('stockData:', stockData)
+    console.log("youuuuu did it ")
+
+    
+     // create the chart
+  //    Highcharts.stockChart('container', {
+
+
+  //     rangeSelector: {
+  //         selected: 2
+  //     },
+
+  //     title: {
+  //         text: 'AAPL Stock Price'
+  //     },
+
+  //     series: [{
+  //         type: 'ohlc',
+  //         name: 'AAPL Stock Price',
+  //         data: stockData,
+
+  //         dataGrouping: {
+  //             units: [[
+  //                 'week', // unit name
+  //                 [1] // allowed multiples
+  //             ], [
+  //                 'month',
+  //                 [1, 2, 3, 4, 6]
+  //             ]]
+  //         }
+  //     }]
+  // });
+
+          
+  }
+  
+  const [stockData, setData] = useState([]);
+  const getData = async () => {
+    
+    try {
+      // let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BYND&outputsize=full&apikey=PVGXY8RVCAKABW50';
+      let url = 'https://demo-live-data.highcharts.com/aapl-ohlcv.json';
+      const res = await fetch(url);
+      console.log('res:', res)
+      const stockdata = await res.json();
+      setData(stockData)
+      console.log('stockdata:', stockdata)
+      testing(stockData)
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+   getData();
+  }, );
+  
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HighchartsReact highcharts={Highcharts} options={{}} />
+  </div>
+    
   );
 }
 
