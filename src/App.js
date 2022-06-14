c3c3e2cd687167fff64d6c1798b54b4065ca1d72
woffordlm <lukewofford@gmail.com>
2022-06-14 16:32:31 -0400

    added chart componenet, added stock component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-// import Highcharts from 'highcharts/highstock'
-// import HighchartsReact from 'highcharts-react-official';
-import Chart from "./components/Chart";
+import Chart from "./components/Chart"
+
 
 
 function App() {
-
-
   const [stockData, setData] = useState(null);
-  
- 
   const getData = async () => {
     
     try {
-      let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BYND&outputsize=full&apikey=PVGXY8RVCAKABW50';
-      // let url = 'https://demo-live-data.highcharts.com/aapl-ohlcv.json';
+      let url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=BYND&outputsize=compact&apikey=PVGXY8RVCAKABW50';
       const res = await fetch(url);
       console.log('res:', res)
       const stockData = await res.json();
       setData(stockData)
       console.log('stockdata:', stockData)
-      // test(stockData)
 
     } catch (error) {
       console.error(error);
@@ -30,15 +23,18 @@ function App() {
 
   useEffect(() => {
    getData();
-  }, []);
+  },[]);
   
 
-  return (
-    
-    <Chart data= {stockData}/>
-    
-    
-  );
+  return (    
+    <div>
+      {stockData ? (
+        <Chart data= {stockData} />
+          ) : (
+           null
+            )}
+    </div> 
+  )
 }
 
 export default App;
